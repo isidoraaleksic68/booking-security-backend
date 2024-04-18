d239ee879cccecd850b07b1614b1bacd5c6477bb
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2024-04-18 16:42:41 +0200

    Initial commit with gitignore and start of project

---
+++ PKI-Service/src/main/java/pki/backend/com/example/PKI/Service/PkiServiceApplication.java
@@ -0,0 +1,13 @@
+package pki.backend.com.example.PKI.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiServiceApplication.class, args);
+	}
+
+}
---
+++ PKI-Service/src/test/java/pki/backend/com/example/PKI/Service/PkiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pki.backend.com.example.PKI.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
